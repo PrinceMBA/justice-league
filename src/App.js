f08f6956da85947b099aa1c3085c918cf5405295
jerome <jeromeprince507@gmail.com>
2023-08-16 13:18:55 +0000

    functional-componengt

--- src/App.js
+++ src/App.js
@@ -34,16 +34,28 @@
 
 // export default App;
 
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Codetrain is awesome, react works!!!!</h1>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Codetrain is awesome, react works!!!!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Codetrain is awesome, react works!!!!</h1>
+    </div>
+  );
+};
 
 export default App;
