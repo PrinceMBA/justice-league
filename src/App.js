afbc77382eea90bb19d3046862a619cafa7b058e
jerome <jeromeprince507@gmail.com>
2023-08-16 13:15:35 +0000

    classs-component

--- src/App.js
+++ src/App.js
@@ -34,28 +34,16 @@
 
 // export default App;
 
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Codetrain is awesome, react works!!!!</h1>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Codetrain is awesome, react works!!!!</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
-
-// import React, { Component } from "react";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>Codetrain is awesome, react works!!!!</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
